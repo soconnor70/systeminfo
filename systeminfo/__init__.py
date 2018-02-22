9039e348aa29581fa3fd1fa6e6dffdde99d28571
Sean O'Connor <seanoconnor.10@hotmail.com>
2018-02-22 09:34:21 +0000

    Initial

---
+++ systeminfo/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for systeminfo."""
+
+__author__ = """Sean OConnor"""
+__email__ = 'seanoconnor.10@hotmail.com'
+__version__ = '0.1.0'
---
+++ systeminfo/systeminfo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+import platform
+def main():
+	return platform.platform()
+
+if __name__ == '__main__':
+	main()
